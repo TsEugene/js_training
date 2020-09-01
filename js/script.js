95ad13f329813271acfdd5169774f2434a02a1b0
TsEugene <eugene.cvet@gmail.com>
2020-09-01 16:48:10 +0500

    create moviesDB

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log(6%2);
+let numberOfFilms = +prompt("How many films?", "");
+console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Last film", ""),
+      b = prompt("Rating", ""),
+      c = prompt("Last film", ""),
+      d = prompt("Rating", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
